f6caacbdc25c986b28fdeaae9584682df22f68dd
Tobias Geistert <geisterttobias@gmail.com>
2021-04-29 09:34:24 +0200

    random Choose added improves readebility

--- mixedMessage.js
+++ mixedMessage.js
@@ -3,18 +3,20 @@ const firstParts = ['You', 'We', 'Humans', 'People', 'Aliens'];
 const secondParts = ['are','will be', 'likely'];
 const thirdParts = ['the best', 'wonderful', 'super', 'tough', 'make it', 'unstoppable'];
 
-
+const randomChoose = partArray => {                     // Choose random parts of an array
+    return partArray[Math.floor(Math.random() * partArray.length)]
+};
 
 const createMessage = (name) => {                                                   //Create a Message with random Items from firstParts, secondParts, thirParts
     let motMessage = '';
 
     if (typeof name === 'string' && name.length >= 2) {                             // If statement checks if you put in a Name than build build a String of random Items
-            motMessage = name + ' ' + secondParts[Math.floor(Math.random() * secondParts.length)] + ' ' + thirdParts[Math.floor(Math.random() * thirdParts.length)] + '!';
+            motMessage = name + ' ' + randomChoose(secondParts) + ' ' + randomChoose(thirdParts) + '!';
         
     } else {
-        motMessage = firstParts[Math.floor(Math.random() * firstParts.length)] + ' ' + secondParts[Math.floor(Math.random() * secondParts.length)] + ' ' + thirdParts[Math.floor(Math.random() * thirdParts.length)] + '!';
+        motMessage = randomChoose(firstParts) + ' ' + randomChoose(secondParts) + ' ' + randomChoose(thirdParts) + '!';
     };
     console.log(motMessage);
-}
+};
 
 createMessage(args[0])
